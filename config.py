bbd57eb3d56fb8bc348a0c1fc1d31032b306b6d2
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-06-23 21:17:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6053049056:AAHg2z3S2WI_36GtVUY3syFnvoycAD3zxCI")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6207952042:AAFlpOdszG-Wq5jvqdjVPfdJCina2klt3QA")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "16246834"))
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "16246834"))
 API_HASH = os.environ.get("API_HASH", "29b3ffa9245c07f05375b92f38e8f13d")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001810853046"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001798450677"))
 
 # OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1715348447"))
@@ -28,13 +28,13 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1715348447"))
 OWNER = os.environ.get("OWNER", "cyella")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://nelbqrpz:jFRl2VgEdG6Xiv5Pn7vrOCT2eJF9iGLT@mel.db.elephantsql.com/nelbqrpz")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://vtlvsxlb:wd8paktb6tcm6bCSs29X4hZVWNbgCMwW@lucky.db.elephantsql.com/vtlvsxlb")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001980712472"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001941542167"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001909730239"))
-FORCE_SUB_GROUP2 = int(os.environ.get("FORCE_SUB_GROUP2", "-1001933177780"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001964317809"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001674393683"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001514378140"))
+FORCE_SUB_GROUP2 = int(os.environ.get("FORCE_SUB_GROUP2", "-1001533559275"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -44,7 +44,7 @@ START_MSG = os.environ.get(
     "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus \n</b>JIKA BOT BERMASALAH HUB @TEKNISI69_BOT.</b>",
 )
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1715348447 1908660708").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1715348447 5224655453").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
 
@@ -63,7 +63,7 @@ DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == "True
 ADMINS.append(OWNER_ID)
 # Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
 # Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
-ADMINS.extend((OWNER_ID, "1715348447"))
+ADMINS.extend((OWNER_ID, "1715348447 5224655453"))
 
 
 LOG_FILE_NAME = "logs.txt"
